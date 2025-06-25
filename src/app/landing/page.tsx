bd4a41ad825939a1983124b845f963cfdf1f0c83
test1234d5x2 <moselsh1@gmail.com>
2025-06-25 19:53:33 +0100

    Got Started With Landing Page

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,22 +1,27 @@
 import Header from "@/components/landing/header";
+import Image from "next/image";
+import landingPage from "@/images/landingImage.jpg"
 
 export default function LandingPage() {
     return (
         <main className="flex flex-col">
             <Header />
-            <section className="flex flex-col items-center justify-center gap-10 h-screen">
+            <section className="flex flex-col items-center justify-center gap-10 min-h-screen p-10">
+                <div className="relative w-full max-w-xl h-64 md:h-96">
+                    <Image src={landingPage} alt="FlashChat" className="rounded-md" fill style={{objectFit: 'cover'}} />
+                </div>
                 <h1 className="text-4xl font-bold">FlashChat</h1>
-                <p className="text-lg text-gray-600">The best way to chat with your friends</p>
-                <section className="flex flex-row gap-4">
-                    <div className="flex flex-col gap-4 w-1/3 bg-gray-100 p-4 rounded-md text-center">
+                <p className="text-lg text-gray-600">The best way to chat with your friends.</p>
+                <section className="flex flex-col md:flex-row gap-4">
+                    <div className="flex flex-col gap-4 w-full bg-gray-100 p-4 rounded-md text-center sm: w-1/3">
                         <h2 className="text-2xl font-bold">Build Your Community</h2>
                         <p className="text-gray-600">Create a space where your users can connect, share, and grow together.</p>
                     </div>
-                    <div className="flex flex-col gap-4 w-1/3 bg-gray-100 p-4 rounded-md text-center">
+                    <div className="flex flex-col gap-4 w-full bg-gray-100 p-4 rounded-md text-center sm: w-1/3">
                         <h2 className="text-2xl font-bold">Share Your Stories</h2>
                         <p className="text-gray-600">Post updates, share photos, and videos with your community.</p>
                     </div>
-                    <div className="flex flex-col gap-4 w-1/3 bg-gray-100 p-4 rounded-md text-center">
+                    <div className="flex flex-col gap-4 w-full bg-gray-100 p-4 rounded-md text-center sm: w-1/3">
                         <h2 className="text-2xl font-bold">Connect with Your Audience</h2>
                         <p className="text-gray-600">Engage with your community and build lasting relationships.</p>
                     </div>
