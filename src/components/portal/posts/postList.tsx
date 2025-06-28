6002a1f3a14c73198d02603bf7c7e8be250c0242
test1234d5x2 <moselsh1@gmail.com>
2025-06-28 18:57:13 +0100

    Fixed Nested Links

--- src/components/portal/posts/postList.tsx
+++ src/components/portal/posts/postList.tsx
@@ -1,16 +1,69 @@
+import Link from "next/link";
 import Post from "./post";
 
 export default function PostList() {
     return (
         <div>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+            <div className="hover:bg-white">
+                <Link href="/portal/post/1">
+                    <Post />
+                </Link>
+            </div>
+            <div className="hover:bg-white">
+                <Link href="/portal/post/2">
+                    <Post />
+                </Link>
+            </div>
+            <div className="hover:bg-white">
+                <Link href="/portal/post/3">
+                    <Post />
+                </Link>
+            </div>
+            <div className="hover:bg-white">
+                <Link href="/portal/post/4">
+                    <Post />
+                </Link>
+            </div>
+            <div className="hover:bg-white">
+                <Link href="/portal/post/5">
+                    <Post />
+                </Link>
+            </div>
+            <div className="hover:bg-white">
+                <Link href="/portal/post/6">
+                    <Post />
+                </Link>
+            </div>
+            <div className="hover:bg-white">
+                <Link href="/portal/post/7">
+                    <Post />
+                </Link>
+            </div>
+            <div className="hover:bg-white">
+                <Link href="/portal/post/8">
+                    <Post />
+                </Link>
+            </div>
+            <div className="hover:bg-white">
+                <Link href="/portal/post/9">
+                    <Post />
+                </Link>
+            </div>
+            <div className="hover:bg-white">
+                <Link href="/portal/post/10">
+                    <Post />
+                </Link>
+            </div>
+            <div className="hover:bg-white">
+                <Link href="/portal/post/11">
+                    <Post />
+                </Link>
+            </div>
+            <div className="hover:bg-white">
+                <Link href="/portal/post/12">
+                    <Post />
+                </Link>
+            </div>
         </div>
     )
 }
